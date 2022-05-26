136965ba12c722e18ff6d8ac8e27094fcf99f74e
Mehul Dave <mehuldave13@gmail.com>
2022-05-26 12:45:29 +0530

    My third commit

---
+++ externaluser-plugin.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: External User 
+ * Plugin URI: 
+ * Description: To get external users from https://jsonplaceholder.typicode.com/users/
+ * Version: 1.0
+ * Author: Mehul Dave
+ * Author URI: 
+ */
+ /**
+ * Function add_my_custom_page
+ * Description: To create a new page and show data from External User api.
+ */
+if(!is_admin()){
+    echo "<h1>Hello from plugin.</h1>";
+}
+?>
